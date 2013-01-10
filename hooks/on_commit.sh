f3cb18f8cfc1599e89198dc43bc90a5d7b28ce6c
Tom <Tom@localhost>
2013-01-10 21:28:38 +0100

    script tweaking

--- hooks/on_commit.sh
+++ hooks/on_commit.sh
@@ -1,11 +1,13 @@
 # NOTE: When executed from hook PWD is the base folder of the repository
 
-# needed for me as the scala installed in bin is too old
+
+
+(# needed for me as the scala installed in bin is too old
 # can be removed is installed version of scala is recent enough
 export PATH=/home/jorquera/Documents/scala/scala-2.10.0-RC5/bin/:$PATH
 
-# generate new pdf
-./build.sh -l manuscrit
+# generate new pdf (discard stdout)
+./build.sh -l manuscrit > /dev/null
 
 # generate stats
 cd "./scripts/phdstats/"
@@ -16,4 +18,4 @@ PATH_TO_PUBLIC_FOLDER="../../../Public/thesis"
 
 cd "-"
 cp ./generated/manuscrit.pdf $PATH_TO_PUBLIC_FOLDER/.
-cp ./scripts/phdstats/phd_progress.pdf $PATH_TO_PUBLIC_FOLDER/.
+cp ./scripts/phdstats/phd_progress.pdf $PATH_TO_PUBLIC_FOLDER/.) &
