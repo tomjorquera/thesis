5ce81c9930f3dad60d7353515737790ec273d6be
Tom <Tom@localhost>
2013-01-11 11:32:33 +0100

    added to plot_stats a part to generate data of the last 7 days

--- scripts/phdstats/plot_stats.sh
+++ scripts/phdstats/plot_stats.sh
@@ -12,3 +12,8 @@ echo "$DATA" | gnuplot config.gnuplot -e "set terminal pdfcairo; set output \"$O
 #draw png
 echo "$DATA" | gnuplot config.gnuplot -e "set terminal png; set output \"$OUTPUT.png\"; plot '-' using 1:2"
 
+#draw png of 7 last days
+CURR_DATE=$(date +"%Y-%m-%d" --date="tomorrow") #add tomorrow to have larger x-axis
+DATE_7DAYS=$(date +"%Y-%m-%d" --date="8 days ago") #add a day before for same reason
+echo "$DATA" | gnuplot config.gnuplot -e "set format x \"%d/%m\";set xrange [\"$DATE_7DAYS\":\"$CURR_DATE\"];set terminal png; set output \""$OUTPUT".7days.png\"; plot '-' using 1:2"
+
