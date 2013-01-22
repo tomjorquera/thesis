41abea534cb9ac06ea5fa52dc89a724174ab8c72
Tom <Tom@localhost>
2013-01-22 17:57:01 +0100

    Scripts: changed plot_stats to also generate a pic of the last 30 days

--- scripts/phdstats/plot_stats.sh
+++ scripts/phdstats/plot_stats.sh
@@ -12,9 +12,11 @@ echo "$DATA" | gnuplot config.gnuplot -e "set terminal pdfcairo; set output \"$O
 #draw png
 echo "$DATA" | gnuplot config.gnuplot -e "set terminal png; set output \"$OUTPUT.all.png\"; plot '-' using 1:2"
 
-#draw png of 7 last and 14 last days
+#draw png of 7, 14 and 30 last days
 CURR_DATE=$(date +"%Y-%m-%d" --date="tomorrow") #add tomorrow to have larger x-axis
 DATE_7DAYS=$(date +"%Y-%m-%d" --date="8 days ago") #add a day before for same reason
 DATE_14DAYS=$(date +"%Y-%m-%d" --date="15 days ago") #add a day before for same reason
+DATE_30DAYS=$(date +"%Y-%m-%d" --date="31 days ago") #add a day before for same reason
 echo "$DATA" | gnuplot config.gnuplot -e "set format x \"%d/%m\";set xrange [\"$DATE_7DAYS\":\"$CURR_DATE\"];set terminal png; set output \""$OUTPUT".7days.png\"; plot '-' using 1:2"
 echo "$DATA" | gnuplot config.gnuplot -e "set format x \"%d/%m\";set xrange [\"$DATE_14DAYS\":\"$CURR_DATE\"];set terminal png; set output \""$OUTPUT".14days.png\"; plot '-' using 1:2"
+echo "$DATA" | gnuplot config.gnuplot -e "set format x \"%d/%m\";set xrange [\"$DATE_30DAYS\":\"$CURR_DATE\"];set terminal png; set output \""$OUTPUT".30days.png\"; plot '-' using 1:2"
