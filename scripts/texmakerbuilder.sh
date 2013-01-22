23964abf509836e709b534e88305a38993850095
Tom <Tom@localhost>
2013-01-22 16:04:54 +0100

    Scripts: changed textmakerbuilder to avoid absolute path (but now require bash magic to work)

--- scripts/texmakerbuilder.sh
+++ scripts/texmakerbuilder.sh
@@ -1,4 +1,12 @@
-DIR="/home/jorquera/Dropbox/Travail/Thèse/manuscrit"
+# Note: the texmaker command should be "bash /path/to/script/texmakerbuilder.sh"
+# else the variable BASH_SOURCE will not be set
 
-cd $DIR
+# this one-liner get the directory where the script is located
+# (from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#go to root folder
+cd $DIR/..
+
+#launch build
 exec ./build.sh -ls manuscrit
