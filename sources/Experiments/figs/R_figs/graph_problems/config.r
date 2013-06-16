b0e9e38309d210ccb652a627570d9084eb7e467e
Tom <Tom@localhost>
2013-06-16 17:06:17 +0200

    Experiments: added some text and figs showing results on generated problem graphs

---
+++ sources/Experiments/figs/R_figs/graph_problems/config.r
@@ -0,0 +1,57 @@
+args <- commandArgs(TRUE)
+
+csv_rand <- "./test_size_random"
+csv_ws <- "./test_size_ws"
+
+#for(name in c(csv_rand, csv_ws)) {
+
+	#csvname <- paste(name, ".csv", sep = "", collapse=NULL)
+
+	#data <- read.csv(csvname, head=FALSE, sep=";")
+	#attach(data)
+	#ordered <- t(data[order(V1),])
+
+	#pdfname <- paste(name, "_degree_boxplot.pdf", sep = "", collapse=NULL)
+	#pdf(file=pdfname)
+	#boxplot(ordered[3:nrow(ordered),],names=ordered[1,])
+	#dev.off()
+
+	#pdfname <- paste(name, "_degree_scatter.pdf", sep = "", collapse=NULL)
+	#pdf(file=pdfname)
+	#plot(ordered[5,],col="blue")
+	#dev.off()
+#}
+
+data_rand <- read.csv(paste(csv_rand, ".csv", sep = "", collapse=NULL), head=FALSE, sep=";")
+attach(data_rand)
+ordered_rand <- t(data_rand[order(V1),])
+
+data_ws <- read.csv(paste(csv_ws, ".csv", sep = "", collapse=NULL), head=FALSE, sep=";")
+attach(data_ws)
+ordered_ws <- t(data_ws[order(V1),])
+
+pdfname <- paste("comp_time.pdf", sep = "", collapse=NULL)
+pdf(file=pdfname)
+par(mar=c(5,5,2,2)) 
+plot(	ordered_rand[1,], ordered_rand[2,]/1000000,
+	col="blue", ann=FALSE, cex=2, cex.axis=2	)
+points(	ordered_rand[1,], ordered_ws[2,]/1000000,
+	col="red",pch=22, ann=FALSE, cex=2)
+title(xlab="agents number", ylab="average time (in ms)", cex.lab=2.4)
+legend(	"topleft",  c("random","small-world"),
+	col=c("blue","red"), pch=21:22, cex=2	);
+
+dev.off()
+
+pdfname <- paste("comp_degree.pdf", sep = "", collapse=NULL)
+pdf(file=pdfname)
+par(mar=c(5,5,2,2)) 
+plot(	ordered_rand[1,], ordered_rand[5,],
+	col="blue", ann=FALSE, cex=2, cex.axis=2	)
+points(	ordered_rand[1,], ordered_ws[5,],
+	col="red",pch=22, ann=FALSE, cex=2)
+title(xlab="nodes number", ylab="mean degree", cex.lab=2.4)
+legend(	"topleft",  c("random","small-world"),
+	col=c("blue","red"), pch=21:22, cex=2	);
+
+dev.off()
---
+++ sources/Experiments/figs/R_figs/graph_problems/config_degree.r
@@ -0,0 +1,19 @@
+csv <- "./test_degree"
+
+data<- read.csv(paste(csv, ".csv", sep = "", collapse=NULL), head=FALSE, sep=";")
+
+pdfname <- "comp_degree.pdf"
+pdf(file=pdfname)
+par(mar=c(5,5,2,2)) 
+plot(data$V5,data$V2/1000000,
+	col="blue", ann=FALSE, cex=2, cex.axis=2	)
+title(xlab="degree", ylab="average time (in ms)", cex.lab=2.4)
+dev.off()
+
+#pdfname <- "comp_degree_avg.pdf"
+#pdf(file=pdfname)
+#par(mar=c(5,5,2,2)) 
+#plot(aggregate(data$V2/1000000, by=list(data$V5), FUN=mean),
+#	col="blue", ann=FALSE, cex=2, cex.axis=2	)
+#title(xlab="degree", ylab="mean average time (in ms)", cex.lab=2.4)
+#dev.off()
