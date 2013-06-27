8d7474591b4e72f460d37c352929e0ea7be5d9ca
Tom <Tom@localhost>
2013-06-27 18:20:02 +0200

    Experiment: updated some data and figures; added some text for comparison with MDO methods

--- sources/Experiments/figs/R_figs/graph_problems/config.r
+++ sources/Experiments/figs/R_figs/graph_problems/config.r
@@ -1,57 +1,62 @@
 args <- commandArgs(TRUE)
 
-csv_rand <- "./test_size_random"
-csv_ws <- "./test_size_ws"
+csv_rand <- "./data_random_eclidean"
+csv_ws <- "./data_watts_strogatz"
 
-#for(name in c(csv_rand, csv_ws)) {
 
-	#csvname <- paste(name, ".csv", sep = "", collapse=NULL)
-
-	#data <- read.csv(csvname, head=FALSE, sep=";")
-	#attach(data)
-	#ordered <- t(data[order(V1),])
-
-	#pdfname <- paste(name, "_degree_boxplot.pdf", sep = "", collapse=NULL)
-	#pdf(file=pdfname)
-	#boxplot(ordered[3:nrow(ordered),],names=ordered[1,])
-	#dev.off()
-
-	#pdfname <- paste(name, "_degree_scatter.pdf", sep = "", collapse=NULL)
-	#pdf(file=pdfname)
-	#plot(ordered[5,],col="blue")
-	#dev.off()
-#}
-
-data_rand <- read.csv(paste(csv_rand, ".csv", sep = "", collapse=NULL), head=FALSE, sep=";")
+data_rand <- read.csv(paste(csv_rand, ".csv", sep = "", collapse=NULL), head=TRUE, sep=";")
 attach(data_rand)
-ordered_rand <- t(data_rand[order(V1),])
+ordered_rand <- data_rand[order(nbAgents),]
 
-data_ws <- read.csv(paste(csv_ws, ".csv", sep = "", collapse=NULL), head=FALSE, sep=";")
+data_ws <- read.csv(paste(csv_ws, ".csv", sep = "", collapse=NULL), head=TRUE, sep=";")
 attach(data_ws)
-ordered_ws <- t(data_ws[order(V1),])
+ordered_ws <- data_ws[order(nbAgents),]
 
-pdfname <- paste("comp_time_by_size.pdf", sep = "", collapse=NULL)
+#RE graphs
+pdfname <- paste("time_by_size_random_eclidean.pdf", sep = "", collapse=NULL)
 pdf(file=pdfname)
-par(mar=c(5,5,2,2)) 
-plot(	ordered_rand[1,], ordered_rand[2,]/1000000,
-	col="blue", ann=FALSE, cex=2, cex.axis=2	)
-points(	ordered_rand[1,], ordered_ws[2,]/1000000,
-	col="red",pch=22, ann=FALSE, cex=2)
-title(xlab="agents number", ylab="average time (in ms)", cex.lab=2.4)
-legend(	"topleft",  c("random euclidean","small-world"),
-	col=c("blue","red"), pch=21:22, cex=2	);
-
+par(mar=c(5,5,2,2)) #margins size
+plot(	ordered_rand[,1], ordered_rand[,2]/1000000,	#data do plot
+	col="blue",pch="x",  ann=FALSE, cex=2, cex.axis=2	)	#display parameters
+title(xlab="agents number", ylab="total time (in ms)", cex.lab=2.4)	#axis labels
 dev.off()
 
-pdfname <- paste("comp_degree_by_size.pdf", sep = "", collapse=NULL)
+#WS graphs
+pdfname <- paste("time_by_size_watts_strogatz.pdf", sep = "", collapse=NULL)
+pdf(file=pdfname)
+par(mar=c(5,5,2,2)) #margins size
+plot(	ordered_ws[,1], ordered_ws[,2]/1000000,
+	col="red",pch="x",  ann=FALSE, cex=2, cex.axis=2	)	#display parameters
+title(xlab="agents number", ylab="total time (in ms)", cex.lab=2.4)	#axis labels
+dev.off()
+
+
+#Degree comparison
+pdfname <- paste("degree_by_size_comparison.pdf", sep = "", collapse=NULL)
 pdf(file=pdfname)
 par(mar=c(5,5,2,2)) 
-plot(	ordered_rand[1,], ordered_rand[5,],
+plot(	ordered_rand[,1], ordered_rand[,5],
 	col="blue", ann=FALSE, cex=2, cex.axis=2	)
-points(	ordered_rand[1,], ordered_ws[5,],
-	col="red",pch=22, ann=FALSE, cex=2)
+points(	ordered_ws[,1], ordered_ws[,5],
+	col="red",pch="x", ann=FALSE, cex=2)
 title(xlab="nodes number", ylab="mean degree", cex.lab=2.4)
 legend(	"topleft",  c("random euclidean","small-world"),
-	col=c("blue","red"), pch=21:22, cex=2	);
+	col=c("blue","red"), pch=c(21,4), cex=2	);
 
 dev.off()
+
+#par(mar=c(5,7,2,2)) #margins size
+
+#plot(	ordered_rand[,1], ordered_rand[,5],	#data do plot
+#	col="blue",pch=".", type="o"
+#	, cex=2, cex.axis=2			#font size
+#	, axes=F, xaxt='n', yaxt='n', xlab="", ylab=""	)
+#axis(2, col='blue', ylim=c(0,max(ordered_rand[,5])), lwd=2, cex.axis=2.4)
+
+#par(new=T)
+#plot(	ordered_ws[,1], ordered_ws[,5],
+#	col="red", pch=".", type="o",  cex=2,
+#	axes=F ,xaxt='n', yaxt='n', xlab=NA, ylab=NA	)
+#axis(2, col='red', ylim=c(0,max(ordered_ws[,5])),lwd=2, cex.axis=2.4, line=3.5)
+#
+#axis(1,pretty(range(ordered_rand[,1]), 10)
--- sources/Experiments/figs/R_figs/graph_problems/config_degree.r
+++ sources/Experiments/figs/R_figs/graph_problems/config_degree.r
@@ -1,13 +1,13 @@
-csv <- "./test_degree"
+csv <- "./data_barabasi_albert"
 
-data<- read.csv(paste(csv, ".csv", sep = "", collapse=NULL), head=FALSE, sep=";")
+data<- read.csv(paste(csv, ".csv", sep = "", collapse=NULL), head=TRUE, sep=";")
 
-pdfname <- "comp_time_by_degree.pdf"
+pdfname <- "time_by_degree_barabasi_albert.pdf"
 pdf(file=pdfname)
 par(mar=c(5,5,2,2)) 
-plot(data$V5,data$V2/1000000,
-	col="blue", ann=FALSE, cex=2, cex.axis=2	)
-title(xlab="mean degree", ylab="average time (in ms)", cex.lab=2.4)
+plot(data$meanDegree,data$totalTime/1000000,
+	ann=FALSE, cex=2, cex.axis=2	)
+title(xlab="mean degree", ylab="total time (in ms)", cex.lab=2.4)
 dev.off()
 
 #pdfname <- "comp_degree_avg.pdf"
